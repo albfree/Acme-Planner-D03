7ba487ecd65e80f67dc166880197390b6f26a7c1
Alberto Guerrero Ramírez <albgueram@alum.us.es>
2021-05-23 19:35:26 +0200

    Cambio no adjuntado en el anterior commit

--- src/test/java/acme/testing/manager/workplan/ManagerWorkPlanUpdateTest.java
+++ src/test/java/acme/testing/manager/workplan/ManagerWorkPlanUpdateTest.java
@@ -26,7 +26,7 @@ public class ManagerWorkPlanUpdateTest extends AcmePlannerTest {
 	@CsvFileSource(resources = "/manager/workplan/update-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
 	public void updatePositive(final int recordIndex, final String title, final String startExecutionPeriod, final String endExecutionPeriod, final String share, final String totalWorkload, final String addTask, final String deleteTask,
-		final String checkTasks) {
+		final String checkTasks, final String newTitle) {
 		super.signIn("manager1", "manager1");
 
 		super.clickOnMenu("Manager", "Work plans list");
@@ -35,7 +35,7 @@ public class ManagerWorkPlanUpdateTest extends AcmePlannerTest {
 
 		super.clickOnListingRecord(recordIndex);
 
-		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("title", newTitle);
 		super.fillInputBoxIn("startExecutionPeriod", startExecutionPeriod);
 		super.fillInputBoxIn("endExecutionPeriod", endExecutionPeriod);
 		super.fillInputBoxIn("share", share);
@@ -54,14 +54,14 @@ public class ManagerWorkPlanUpdateTest extends AcmePlannerTest {
 
 		super.clickOnSubmitButton("Update");
 
-		super.checkColumnHasValue(recordIndex, 0, title);
+		super.checkColumnHasValue(recordIndex, 0, newTitle);
 		super.checkColumnHasValue(recordIndex, 1, startExecutionPeriod);
 		super.checkColumnHasValue(recordIndex, 2, endExecutionPeriod);
 		super.checkColumnHasValue(recordIndex, 3, share);
 
 		super.clickOnListingRecord(recordIndex);
 
-		super.checkInputBoxHasValue("title", title);
+		super.checkInputBoxHasValue("title", newTitle);
 		super.checkInputBoxHasValue("startExecutionPeriod", startExecutionPeriod);
 		super.checkInputBoxHasValue("endExecutionPeriod", endExecutionPeriod);
 		super.checkInputBoxHasValue("share", share);
@@ -105,7 +105,7 @@ public class ManagerWorkPlanUpdateTest extends AcmePlannerTest {
 	@ParameterizedTest
 	@CsvFileSource(resources = "/manager/workplan/update-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(20)
-	public void updateNegative(final int recordIndex, final String title, final String startExecutionPeriod, final String endExecutionPeriod, final String share, final String addTask) {
+	public void updateNegative(final int recordIndex, final String title, final String startExecutionPeriod, final String endExecutionPeriod, final String share, final String addTask, final String newTitle) {
 		super.signIn("manager1", "manager1");
 
 		super.clickOnMenu("Manager", "Work plans list");
@@ -114,7 +114,7 @@ public class ManagerWorkPlanUpdateTest extends AcmePlannerTest {
 		
 		super.clickOnListingRecord(recordIndex);
 		
-		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("title", newTitle);
 		super.fillInputBoxIn("startExecutionPeriod", startExecutionPeriod);
 		super.fillInputBoxIn("endExecutionPeriod", endExecutionPeriod);
 		super.fillInputBoxIn("share", share);
